7b7f33196d511565d070394518f30ffb1637b58b
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-01-01 21:33:15 -0500

    Added function for filtering feature data and to normalize each feature values.

---
+++ graph.py
@@ -0,0 +1,95 @@
+"""Evaluating the impact of COVID-19 on Canadians’ spending habits: frontend module
+
+This module is responsible for loading, filtering and displaying data using plotly.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2021 Ajinkya Bhosale.
+"""
+from datetime import date
+import plotly.express as px
+import plotly.io as pio
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from backend import load_data
+
+# Load in all the data available(including the predicted data from Mar 2020 to Oct 2021)
+main_data = load_data(date(2020, 3, 1), date(2021, 10, 1), True)
+main_data = pd.DataFrame(main_data)  # turned the data into DataFrame object
+
+
+def get_filtered_data(start_date: date, end_date: date) -> pd.DataFrame:
+    """Returns a copy of the data for a selected date range
+
+    The 'main-data' is fetched once everytime the program starts. If a user would like to
+    shorten/change the date range to be displayed in the graph, a 'copy' is made out of the
+    main_data to avoid computing it over and over.
+
+    Sample usage:
+    To filter data from March 2020 to February 2021:
+    >>> start_dt = date(2020, 3, 1)
+    >>> end_dt = date(2021, 2, 1)
+    >>> filtered_data = get_filtered_data(start_dt, end_dt)
+    """
+    filtered_data = main_data.loc[(main_data['date'] >= start_date)
+                                  & (main_data['date'] <= end_date)]
+
+    return filtered_data
+
+
+def normalize_data(filtered_data: pd.DataFrame, usr_choice: list) -> object:
+    """Return the attribute values that are normalized form the provided filtered_data and
+    a list containing attribute names to normalize.
+    The data is normalized on a scale of 0-10.
+
+    The returned data is a nested list containing list of normalized values for each attribute.
+    The list preserves odrer: the index of in the input list corrosponds to the index of the
+    nested list from the output
+
+    Sample Usage:
+    To normalize values to Covid Cases and CPI.
+    >>> start_dt = date(2020, 3, 1)
+    >>> end_dt = date(2021, 2, 1)
+    >>> filtered_data = get_filtered_data(start_dt, end_dt)
+    >>> normalize_data(filtered_data, ['covid_cases', 'cpi'])
+    """
+    scaling = MinMaxScaler((0, 10))
+    list_so_far = list()
+    for choice in usr_choice:
+        if choice == 'covid_cases':
+            arr = scaling.fit_transform(filtered_data['covid_cases'].values.reshape(-1, 1))
+            temp = np.reshape(arr, arr.__len__())
+            list_so_far.append(temp.tolist())
+        elif choice == 'unemployment_rate':
+            arr = scaling.fit_transform(filtered_data['unemployment_rate'].values.reshape(-1, 1))
+            temp = np.reshape(arr, arr.__len__())
+            list_so_far.append(temp.tolist())
+        elif choice == 'csi':
+            arr = scaling.fit_transform(filtered_data.csi.str['Total'].values.reshape(-1, 1))
+            temp = np.reshape(arr, arr.__len__())
+            list_so_far.append(temp.tolist())
+        elif choice == 'cpi':
+            arr = scaling.fit_transform(filtered_data.cpi.str['Allitems'].values.reshape(-1, 1))
+            temp = np.reshape(arr, arr.__len__())
+            list_so_far.append(temp.tolist())
+
+    return list_so_far
+
+
+if __name__ == '__main__':
+    import python_ta
+    import python_ta.contracts
+
+    python_ta.contracts.check_all_contracts()
+    python_ta.check_all(config={
+        'extra-imports': ['datetime', 'pandas', 'plotly', 'plotly.io', 'plotly.express', 'numpy',
+                          'sklearn.preprocessing', 'backend'],
+        'allowed-io': ['load_csv'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
+
+    import doctest
+
+    doctest.testmod()
